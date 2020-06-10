9c25ddc876c4e0fb0b941c24f8d6ffbe8b0d7e06
Renan <renancleyson.f@gmail.com>
2020-06-10 16:21:23 -0300

    created test for Timer component

--- src/__tests__/Timer.tsx
+++ src/__tests__/Timer.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render } from 'react-dom';
 import moment from 'moment';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import Timer from '../Tracker/Timer';
 
+jest.useFakeTimers();
+
+let container: HTMLDivElement;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
 it('renders without crashing', (): void => {
-  const div = document.createElement('div');
-  render(<Timer startTime={moment()} isTracking={false} />, div);
+  act(() => {
+    render(<Timer startTime={moment()} isTracking={false} />, container);
+  });
+});
+
+it('manages time track by clicks', (): void => {
+  const startTime = moment().subtract(1, 'hours').subtract(25, 'minutes').subtract(5, 'seconds');
+  act(() => {
+    render(<Timer isTracking startTime={startTime} />, container);
+  });
+
+  const timerDisplay: HTMLSpanElement | null = document.querySelector(
+    '[data-testid=timer-content]'
+  );
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
+  expect(timerDisplay?.innerHTML).toBe('1:25:05');
 });
