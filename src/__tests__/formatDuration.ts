039207416e49fc0b46bf71144d8d01192bff0dad
Renan <renancleyson.f@gmail.com>
2020-06-13 19:56:52 -0300

    test to formatDuration helper created

---
+++ src/__tests__/formatDuration.ts
@@ -0,0 +1,32 @@
+import moment from 'moment';
+import formatDuration from '../helpers/formatDuration';
+
+it('formats a Duration with less than one hour to MM:SS', (): void => {
+  const formattedDuration = formatDuration(
+    moment.duration({
+      minutes: 20,
+      seconds: 10,
+    })
+  );
+  expect(formattedDuration).toBe('20:10');
+});
+
+it('formats a Duration with greater than one hour to H:MM:SS', (): void => {
+  let formattedDuration = formatDuration(
+    moment.duration({
+      hours: 2,
+      minutes: 20,
+      seconds: 10,
+    })
+  );
+  expect(formattedDuration).toBe('2:20:10');
+
+  formattedDuration = formatDuration(
+    moment.duration({
+      hours: 10,
+      minutes: 20,
+      seconds: 10,
+    })
+  );
+  expect(formattedDuration).toBe('10:20:10');
+});
