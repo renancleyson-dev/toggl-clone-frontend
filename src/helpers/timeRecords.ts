66333c7327f48a282f8dd2c1342693e65684aa72
Renan <renancleyson.f@gmail.com>
2020-06-17 20:27:19 -0300

    ids are treated as numbers now

--- src/helpers/timeRecords.ts
+++ src/helpers/timeRecords.ts
@@ -3,52 +3,50 @@ import axios from 'axios';
 interface ITimeRecord {
   startTime: string;
   endTime: string;
-  timeRecordId: string;
+  label: string;
+  category: string;
+  timeRecordId: number;
 }
 
 export const fetchTimeRecord = (
   page: number,
   perPage: number,
-  userId: string
-): Promise<ITimeRecord> => {
-  return axios
+  userId: number
+): Promise<ITimeRecord> =>
+  axios
     .get(`users/${userId}/time_records`, {
       params: { page, per_page: perPage },
     })
     .then((response) => response.data)
     .catch((error) => error);
-};
 
 export const createTimeRecord = (
   startTime: string,
   endTime: string,
-  userId: string
-): Promise<void | Error> => {
-  return axios
+  userId: number
+): Promise<void | Error> =>
+  axios
     .post(`users/${userId}/time_records`, {
       params: { start_time: startTime, end_time: endTime },
     })
     .then(() => {})
     .catch((error) => error);
-};
 
 export const updateTimeRecord = (
   startTime: string,
   endTime: string,
-  userId: string,
-  timeRecordId: string
-): Promise<void | Error> => {
-  return axios
+  userId: number,
+  timeRecordId: number
+): Promise<void | Error> =>
+  axios
     .put(`users/${userId}/time_records/${timeRecordId}`, {
       params: { start_time: startTime, end_time: endTime },
     })
     .then(() => {})
     .catch((error) => error);
-};
 
-export const deleteTimeRecord = (userId: string, timeRecordId: string): Promise<void | Error> => {
-  return axios
+export const deleteTimeRecord = (userId: number, timeRecordId: number): Promise<void | Error> =>
+  axios
     .delete(`users/${userId}/time_records/${timeRecordId}`)
     .then(() => {})
     .catch((error) => error);
-};
--- src/helpers/users.ts
+++ src/helpers/users.ts
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 interface IUser {
-  id: string;
+  id: number;
   name: string;
   email: string;
 }
 
-export const fetchUser = (userId: string): Promise<IUser> => {
+export const fetchUser = (userId: number): Promise<IUser> => {
   return axios
     .get(`users/${userId}`)
     .then((response) => response.data)
     .catch((error) => error);
 };
 
-export const updateUser = (userId: string): Promise<void | Error> => {
+export const updateUser = (userId: number): Promise<void | Error> => {
   return axios
     .put(`users/${userId}`)
     .then(() => {})
     .catch((error) => error);
 };
 
-export const deleteUser = (userId: string): Promise<void | Error> => {
+export const deleteUser = (userId: number): Promise<void | Error> => {
   return axios
     .delete(`users/${userId}`)
     .then(() => {})
