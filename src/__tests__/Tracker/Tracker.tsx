a5a02754d0decfbd1c022f942f03f32644cd2ffb
Renan <renancleyson.f@gmail.com>
2020-06-18 16:06:16 -0300

    removed unnecessary code

--- src/__tests__/Tracker/Tracker.tsx
+++ src/__tests__/Tracker/Tracker.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import moment from 'moment';
 import MockedTrackContext from '../../mocks/MockedTrackContext';
 import MockedUserContext from '../../mocks/MockedUserContext';
 import Tracker from '../../Tracker/Tracker';
@@ -17,13 +16,11 @@ afterEach(() => {
   container.remove();
 });
 
-const mockedUser = { id: 1, name: 'renan', email: 'a@gmail.com' };
-
 it('renders without crashing', (): void => {
   act(() => {
     render(
-      <MockedUserContext user={mockedUser}>
-        <MockedTrackContext isTracking={false} startTime={moment()}>
+      <MockedUserContext>
+        <MockedTrackContext>
           <Tracker />
         </MockedTrackContext>
       </MockedUserContext>,
