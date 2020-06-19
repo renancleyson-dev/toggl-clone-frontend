7ec1017978b30d1e02aa9015789652dd1820dd33
Renan <renancleyson.f@gmail.com>
2020-06-19 16:56:45 -0300

    file to create form styles

---
+++ src/formStyles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TextInput = styled.input.attrs(() => ({
+  type: 'text',
+}))``;
+export const SubmitButton = styled.input.attrs(() => ({
+  type: 'submit',
+}))``;
+export const Button = styled.input.attrs(() => ({
+  type: 'button',
+}))``;
