8bd4b45e9c1d84dd21f2fa3bf30c0c3b2d9ab520
Renan <renancleyson.f@gmail.com>
2020-06-22 10:37:46 -0300

    ManualTimer code refactored

--- src/History/ManualTimer.tsx
+++ src/History/ManualTimer.tsx
@@ -6,51 +6,68 @@ import { toMoment, userFormat } from '../helpers/timeFormats';
 import { MANUAL_TRACKER_ICON } from '../helpers/constants';
 import TimeRecordEditor from '../Components/TimeRecordEditor';
 
-interface Props {
-  id?: number;
-  startTime?: moment.Moment;
-  endTime?: moment.Moment;
-}
-
-const getDuration = (startTime?: moment.Moment, endTime?: moment.Moment) => {
+const getDuration = (startTime: string, endTime: string) => {
   const startTimeMoment = moment(startTime, toMoment);
   const endTimeMoment = moment(endTime, toMoment);
 
   return moment.duration(endTimeMoment.diff(startTimeMoment));
 };
 
+const setMomentTime = (timeMoment: moment.Moment, timeString: string) => {
+  return moment(timeString, toMoment)
+    .add(timeMoment.year())
+    .add(timeMoment.month())
+    .add(timeMoment.day());
+};
+
+const ShowButton = ({
+  setIsOpen,
+}: {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => (
+  <button type="button" onClick={() => setIsOpen((prevState: boolean) => !prevState)}>
+    <img src={MANUAL_TRACKER_ICON} alt="Editor" />
+  </button>
+);
+
+interface Props {
+  id?: number;
+  startTime: moment.Moment;
+  endTime: moment.Moment;
+}
+
 // UI to manage the start and end time of a record
 export default function ManualTimer({ id, startTime, endTime }: Props) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [timeRecordStartTime, setTimeRecordStartTime] = useState<string>();
-  const [timeRecordEndTime, setTimeRecordEndTime] = useState<string>();
+  const [timeRecordStartTime, setTimeRecordStartTime] = useState<string>('');
+  const [timeRecordEndTime, setTimeRecordEndTime] = useState<string>('');
   const { user } = useContext(UserContext);
   useEffect(() => {
     if (!(timeRecordStartTime && timeRecordEndTime)) {
-      setTimeRecordStartTime(startTime?.format(userFormat));
-      setTimeRecordEndTime(endTime?.format(userFormat));
+      setTimeRecordStartTime(startTime.format(userFormat));
+      setTimeRecordEndTime(endTime.format(userFormat));
     }
     if (!isOpen && id) {
-      const startTime = moment(timeRecordStartTime, toMoment).format();
-      const endTime = moment(timeRecordEndTime, toMoment).format();
-      updateTimeRecord(user.id, id, { startTime, endTime });
+      const newStartTime = setMomentTime(startTime, timeRecordStartTime).format();
+      const newEndTime = setMomentTime(endTime, timeRecordEndTime).format();
+      updateTimeRecord(user.id, id, { startTime: newStartTime, endTime: newEndTime });
     }
   }, [timeRecordStartTime, timeRecordEndTime, startTime, endTime, user, id, isOpen]);
 
   if (!isOpen) {
-    return (
-      <button type="button" onClick={() => setIsOpen((prevState: boolean) => !prevState)}>
-        <img src={MANUAL_TRACKER_ICON} alt="Editor" />
-      </button>
-    );
+    return <ShowButton setIsOpen={setIsOpen} />;
   }
   return (
-    <TimeRecordEditor
-      timeRecordStartTime={timeRecordStartTime}
-      timeRecordEndTime={timeRecordEndTime}
-      setTimeRecordStartTime={setTimeRecordStartTime}
-      setTimeRecordEndTime={setTimeRecordEndTime}
-      duration={getDuration(startTime, endTime)}
-    />
+    <>
+      <ShowButton setIsOpen={setIsOpen} />
+      <TimeRecordEditor
+        timeRecordStartTime={timeRecordStartTime}
+        timeRecordEndTime={timeRecordEndTime}
+        setTimeRecordStartTime={setTimeRecordStartTime}
+        setTimeRecordEndTime={setTimeRecordEndTime}
+        duration={getDuration(timeRecordStartTime, timeRecordEndTime)}
+        timeData={{ startTime, endTime }}
+      />
+    </>
   );
 }
