daf399d118873b7fd57b1c71f8e8bfec111a7347
Renan <renancleyson.f@gmail.com>
2020-06-22 10:45:38 -0300

    uncommited code for DateSelect component icons paths

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,5 @@
 export const START_BUTTON_ICON = '/assets/images/startButton.svg';
 export const STOP_BUTTON_ICON = '/assets/images/stopButton.svg';
+export const NEXT_BUTTON_ICON = '/assets/images/nextButton.svg';
+export const PREVIOUS_BUTTON_ICON = '/assets/images/previousButton.svg';
+export const MANUAL_TRACKER_ICON = '/assets/images/manualTracker.svg';
