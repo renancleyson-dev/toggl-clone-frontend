5d506e51ef43db8248dd3f45e66ff857cfe4468c
Renan <renancleyson.f@gmail.com>
2020-06-29 00:02:45 -0300

    formatter for duration shows a 00:00 output for negative times

--- src/helpers/formatDuration.ts
+++ src/helpers/formatDuration.ts
@@ -3,15 +3,19 @@ import moment from 'moment';
 export default (duration: moment.Duration): string => {
   const formattedArray = [`${duration.minutes()}`, `${duration.seconds()}`];
 
-  if (duration.minutes() < 10) {
+  if (duration.minutes() < 0) {
+    formattedArray[0] = '00';
+  } else if (duration.minutes() < 10) {
     formattedArray[0] = `0${duration.minutes()}`;
   }
 
-  if (duration.seconds() < 10) {
+  if (duration.seconds() < 0) {
+    formattedArray[1] = '00';
+  } else if (duration.seconds() < 10) {
     formattedArray[1] = `0${duration.seconds()}`;
   }
 
-  if (duration.hours()) {
+  if (duration.hours() && duration.hours() >= 0) {
     formattedArray.unshift(`${duration.hours()}`);
   }
 
