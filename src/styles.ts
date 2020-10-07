28af78ac86ef19695124fe8d2eb47445f58058b7
Renan <renancleyson.f@gmail.com>
2020-10-07 08:43:24 -0300

    Create reusable styles

---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import { css } from 'styled-components';
+
+export const colors = {
+  primary: '#e57cd8',
+
+  pinkLight: '#fce5d8',
+
+  backgroundDarkPrimary: '#564260',
+  backgroundDark: '#2c1338',
+  backgroundMedium: '#412a4c',
+};
+
+export const formContainer = css`
+  margin-top: 10px;
+  padding-left: 15px;
+  border: solid 2px #aeaeae;
+  height: 50px;
+  font-size: 14px;
+  font-family: 'Inter', sans-serif;
+`;
