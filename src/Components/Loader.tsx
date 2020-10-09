08fa453c7c7a2ecb19fdbbe19a6d123bce426732
Renan <renancleyson.f@gmail.com>
2020-10-09 14:13:20 -0300

    Create a Loader component

---
+++ src/Components/Loader.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { colors } from '../styles';
+import Logo from './Logo';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  background-color: ${colors.backgroundDark};
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoWrapper = styled.div`
+  width: 170px;
+  margin-bottom: 20px;
+`;
+
+const Spinner = styled.div`
+  width: 25px;
+  height: 25px;
+  border: 3px solid transparent;
+  border-bottom: 3px solid ${colors.primary};
+  border-radius: 50%;
+
+  animation: ${rotate} 0.5s linear infinite;
+`;
+
+export default function Loader() {
+  return (
+    <LoaderWrapper>
+      <LogoWrapper>
+        <Logo />
+      </LogoWrapper>
+      <Spinner />
+    </LoaderWrapper>
+  );
+}
