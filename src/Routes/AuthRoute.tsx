f19ddcd04f897c213c814c1f9319929551c74de3
Renan <renancleyson.f@gmail.com>
2020-10-09 09:41:34 -0300

    Add Header to authentication route

--- src/Routes/AuthRoute.tsx
+++ src/Routes/AuthRoute.tsx
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 import { Wrapper, FormBoxWrapper } from '../Auth/Styles';
 import Login from '../Auth/Login';
 import Register from '../Auth/Register';
+import Header from '../Auth/Header';
 
 export default function AuthRoute() {
   return (
     <Wrapper>
+      <Header />
       <FormBoxWrapper>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
