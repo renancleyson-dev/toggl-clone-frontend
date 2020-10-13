a0dd88d3ed1663ef572c4e3ab59dcb3f80adb7c6
Renan <renancleyson.f@gmail.com>
2020-10-13 14:04:20 -0300

    Add loader when loading user

--- src/Components/PrivateRoute.tsx
+++ src/Components/PrivateRoute.tsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 import { USER_KEY } from '../helpers/constants';
 import { UserContext } from '../Contexts/UserContext';
+import Loader from './Loader';
 
 export default function PrivateRoute({ component: Component, ...rest }: RouteProps) {
   const userId = localStorage.getItem(USER_KEY);
   const { user } = useContext(UserContext);
 
   if (!Component || (userId && !user.id)) {
-    return null;
+    return <Loader />;
   }
 
   return (
