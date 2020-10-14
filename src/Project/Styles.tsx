66362487f12be20fecfb1513fdd2fd152ed9d1d9
Renan <renancleyson.f@gmail.com>
2020-10-14 08:00:11 -0300

    Move formStyles to Project folder

--- src/Project/Styles.tsx
+++ src/Project/Styles.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
-export const TextInput = styled.input.attrs(() => ({
-  type: 'text',
-}))``;
+export const TextInput = styled.input``;
 export const SubmitButton = styled.input.attrs(() => ({
   type: 'submit',
 }))``;
 export const Button = styled.input.attrs(() => ({
   type: 'button',
 }))``;
+
+export const LabelInput = styled(TextInput)`
+  flex: 1 1 100%;
+`;
