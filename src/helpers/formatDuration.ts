7776f5eaabc535a4a2bb0cdb7c85ae3f9a7b989d
Renan <renancleyson.f@gmail.com>
2020-10-17 12:10:25 -0300

    Change UI format of duration

--- src/helpers/formatDuration.ts
+++ src/helpers/formatDuration.ts
@@ -1,22 +1,26 @@
 import moment from 'moment';
 
 export default (duration: moment.Duration): string => {
-  const formattedArray = [`${duration.minutes()}`, `${duration.seconds()}`];
+  const formattedArray = [
+    `${duration.hours()}`,
+    `${duration.minutes()}`,
+    `${duration.seconds()}`,
+  ];
+
+  if (duration.hours() && duration.hours() >= 0) {
+    formattedArray[0] = `${duration.hours()}`;
+  }
 
   if (duration.minutes() < 0) {
-    formattedArray[0] = '00';
+    formattedArray[1] = '00';
   } else if (duration.minutes() < 10) {
-    formattedArray[0] = `0${duration.minutes()}`;
+    formattedArray[1] = `0${duration.minutes()}`;
   }
 
   if (duration.seconds() < 0) {
-    formattedArray[1] = '00';
+    formattedArray[2] = '00';
   } else if (duration.seconds() < 10) {
-    formattedArray[1] = `0${duration.seconds()}`;
-  }
-
-  if (duration.hours() && duration.hours() >= 0) {
-    formattedArray.unshift(`${duration.hours()}`);
+    formattedArray[2] = `0${duration.seconds()}`;
   }
 
   return formattedArray.join(':');
