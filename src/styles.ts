c8021ce374a466a16334451fceffbd75fb29a432
Renan <renancleyson.f@gmail.com>
2020-10-21 20:55:05 -0300

    Add reusable styles
    The styles added will be used to handle boxes that will be stacked over
    any element

--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 export const colors = {
   primary: '#e57cd8',
 
@@ -7,3 +9,21 @@ export const colors = {
   backgroundDarkPrimary: '#564260',
   backgroundMedium: '#412a4c',
 };
+
+export const AddButtonWrapper = styled.div`
+  min-width: 100%;
+  padding: 10px 0;
+  border-top: 1px solid rgb(251, 229, 247);
+  text-align: center;
+`;
+
+export const StackedBox = styled.div`
+  position: absolute;
+  top: 35px;
+  right: -130px;
+  padding-top: 15px;
+  box-shadow: 0px 0px 7px 1px #ccc;
+  border-radius: 8px;
+  background-color: #fff;
+  z-index: 100;
+`;
