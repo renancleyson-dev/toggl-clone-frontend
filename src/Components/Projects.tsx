2d7ad61d263a71a65015ec280415807b204cd86d
Renan <renancleyson.f@gmail.com>
2020-10-31 11:21:27 -0300

    Add projects list to Projects component

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import styled from 'styled-components';
 import Modal from 'react-modal';
 import { RiFolder2Fill, RiAddFill } from 'react-icons/ri';
+import { TrackContext } from '../Contexts/TrackContext';
+import { IProject } from '../types/projects';
 import { positionedModalStyles, AddButtonWrapper, colors, InputStyles } from '../styles';
 import SearchInput from './SearchInput';
 import CreateProjectModal from './CreateProjectModal';
@@ -24,6 +26,13 @@ const Input = styled.input`
   ${InputStyles}
 `;
 
+const AddButtonIcon = styled.span`
+  font-size: 20px;
+  color: ${colors.primary};
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
 const FolderIconWrapper = styled.div`
   color: ${({ showBox }: { showBox: boolean }) => (showBox ? colors.primary : '#a1a1a1')};
   cursor: pointer;
@@ -34,20 +43,72 @@ const FolderIconWrapper = styled.div`
   }
 `;
 
-const ProjectsList = styled.ul`
+const ProjectsListWrapper = styled.ul`
   list-style-type: none;
   flex: 1 1 100%;
-  min-height: 300px;
-  margin: 0;
-  padding: 0;
+  min-height: 100px;
+  max-height: 340px;
+  margin: 20px 0 10px;
+  padding: 0 5px;
+  overflow: auto;
 `;
 
-const AddButtonIcon = styled.span`
-  font-size: 20px;
-  color: ${colors.primary};
-  margin-right: 5px;
+const ProjectItem = styled.li`
   cursor: pointer;
+  padding: 5px 10px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  background-color: ${({ hovered }: { hovered: boolean }) =>
+    hovered ? ' #f1f1f1' : '#fff'};
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
 `;
+
+const ProjectName = styled.span`
+  color: ${({ color }: { color: string }) => color};
+`;
+
+const MiniColorCircle = styled.div`
+  width: 8px;
+  height: 8px;
+  line-height: 0;
+  margin-right: 5px;
+  border-radius: 50%;
+  background-color: ${({ color }: { color: string }) => color};
+`;
+
+const ProjectsList = () => {
+  const [lastHovered, setLastHovered] = useState<number>();
+  const { projects } = useContext(TrackContext);
+
+  const projectItems = projects.map(({ id, name, color }) => (
+    <ProjectItem
+      key={id}
+      hovered={lastHovered === id}
+      onMouseOver={() => setLastHovered(id)}
+    >
+      <MiniColorCircle color={color} />
+      <ProjectName color={color}>{name}</ProjectName>
+    </ProjectItem>
+  ));
+
+  return (
+    <ProjectsListWrapper>
+      <ProjectItem
+        key={0}
+        hovered={lastHovered === 0}
+        onMouseOver={() => setLastHovered(0)}
+      >
+        <MiniColorCircle color="#aaa" />
+        <ProjectName color="#000">No Project</ProjectName>
+      </ProjectItem>
+      {projectItems}
+    </ProjectsListWrapper>
+  );
+};
 
 interface ProjectsBoxProps extends Modal.Props {
   iconRef: React.MutableRefObject<any>;
@@ -61,19 +122,19 @@ const ProjectsBox = ({
   setIsOpen,
   ...modalProps
 }: ProjectsBoxProps) => {
-  const [position, setPosition] = useState({
-    top: '0',
-    left: '0',
-  });
+  const [position, setPosition] = useState({ top: '0', left: '0' });
   const updatedProjectModalStyles = {
     overlay: projectModalStyles.overlay,
     content: { ...projectModalStyles.content, ...position },
+  };
+  const handleAddButtonClick = () => {
+    setIsCreateModalOpen(true);
+    setIsOpen(false);
   };
 
   useEffect(() => {
     if (iconRef.current instanceof Element && modalProps.isOpen) {
       const { top, left } = iconRef.current.getBoundingClientRect();
-
       setPosition({
         top: `${top + window.scrollY + 35}px`,
         left: `${left + window.scrollX - 100}px`,
@@ -87,12 +148,7 @@ const ProjectsBox = ({
         <Input autoFocus placeholder="Find project..." />
       </SearchInput>
       <ProjectsList />
-      <AddButtonWrapper
-        onClick={() => {
-          setIsCreateModalOpen(true);
-          setIsOpen(false);
-        }}
-      >
+      <AddButtonWrapper onClick={handleAddButtonClick}>
         <AddButtonIcon>
           <RiAddFill />
         </AddButtonIcon>
@@ -102,10 +158,11 @@ const ProjectsBox = ({
   );
 };
 
-export default function Projects() {
+export default function Projects({ actualProject }: { actualProject?: IProject }) {
   const [showBox, setShowBox] = useState(false);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const iconRef = useRef(null);
+  const { setProjects } = useContext(TrackContext);
 
   return (
     <>
@@ -116,9 +173,10 @@ export default function Projects() {
           setShowBox(true);
         }}
       >
-        <RiFolder2Fill />
+        {actualProject ? <div>{actualProject.name}</div> : <RiFolder2Fill />}
       </FolderIconWrapper>
       <CreateProjectModal
+        onCreateProject={(project) => setProjects((prevState) => [...prevState, project])}
         isOpen={isCreateModalOpen}
         setIsOpen={setIsCreateModalOpen}
         onRequestClose={() => setIsCreateModalOpen(false)}
